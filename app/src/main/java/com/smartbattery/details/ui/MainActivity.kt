3c4fd743ce68754ea8248d664e5a9ef5b28b4789
kajal gupta <kj27july@gmail.com>
2021-12-13 11:04:02 +0530

    added default values

--- app/src/main/java/com/smartbattery/details/ui/MainActivity.kt
+++ app/src/main/java/com/smartbattery/details/ui/MainActivity.kt
@@ -53,34 +53,35 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun handleBatteryIntent(intent: Intent) {
-        val mfg = intent.extras!!.getString("mfg")
-        val partnumber = intent.extras!!.getString("partnumber")
-        val serialnumber = intent.extras!!.getString("serialnumber")
-        val ratedcapacity = intent.extras!!.getInt("ratedcapacity")
-        val battery_decommission = intent.extras!!.getInt("battery_decommission")
-        val total_cumulative_charge = intent.extras!!.getInt("total_cumulative_charge")
+        val mfg = intent.extras!!.getString("mfg", "unknown")
+        val partnumber = intent.extras!!.getString("partnumber", "unknown")
+        val serialnumber = intent.extras!!.getString("serialnumber", "unknown")
+        val ratedcapacity = intent.extras!!.getInt("ratedcapacity", -1)
+        val battery_decommission = intent.extras!!.getInt("battery_decommission", -1)
+        val total_cumulative_charge = intent.extras!!.getInt("total_cumulative_charge", -1)
         if (getBatteryType(intent) == 202 || getBatteryType(intent) == 206) {
-            battery_usage_numb = intent.extras!!.getInt("battery_usage_numb").toString()
+            battery_usage_numb = intent.extras!!.getInt("battery_usage_numb", -1).toString()
         }
 
         if (getBatteryType(intent) == 201) {
-            base_cumulative_charge = intent.extras!!.getInt("base_cumulative_charge").toString()
-            seconds_since_first_use = intent.extras!!.getInt("seconds_since_first_use").toString()
-            present_capacity = intent.extras!!.getInt("present_capacity").toString()
-            health_percentage = intent.extras!!.getInt("health_percentage").toString()
-            time_to_empty = intent.extras!!.getInt("time_to_empty").toString()
-            time_to_full = intent.extras!!.getInt("time_to_full").toString()
-            present_charge = intent.extras!!.getInt("present_charge").toString()
+            base_cumulative_charge = intent.extras!!.getInt("base_cumulative_charge", -1).toString()
+            seconds_since_first_use =
+                intent.extras!!.getInt("seconds_since_first_use", -1).toString()
+            present_capacity = intent.extras!!.getInt("present_capacity", -1).toString()
+            health_percentage = intent.extras!!.getInt("health_percentage", -1).toString()
+            time_to_empty = intent.extras!!.getInt("time_to_empty", -1).toString()
+            time_to_full = intent.extras!!.getInt("time_to_full", -1).toString()
+            present_charge = intent.extras!!.getInt("present_charge", -1).toString()
         }
         apiList.clear()
 
         apiList.add("=== STANDARD API ===")
-        apiList.add("Manufacture Date: " + mfg!!)
-        apiList.add("Serial Number: " + serialnumber!!)
+        apiList.add("Manufacture Date: " + mfg)
+        apiList.add("Serial Number: " + serialnumber)
         apiList.add("Health percentage: " + health_percentage)
 
         apiList.add("===NON STANDARD API ===")
-        apiList.add("Part number: " + partnumber!!)
+        apiList.add("Part number: " + partnumber)
         apiList.add("Rated capacity: " + ratedcapacity)
         apiList.add("Battery decommission: " + battery_decommission)
         apiList.add("Total cumulative charge: " + total_cumulative_charge)
