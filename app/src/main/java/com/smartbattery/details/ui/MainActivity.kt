5b387a40e21ddc0e9d4ebf7372d2005216f291a2
kajal gupta <kj27july@gmail.com>
2021-12-14 12:44:45 +0530

    Table_UI

--- app/src/main/java/com/smartbattery/details/ui/MainActivity.kt
+++ app/src/main/java/com/smartbattery/details/ui/MainActivity.kt
@@ -5,62 +5,55 @@ import android.content.Intent
 import android.content.IntentFilter
 import android.os.Build
 import android.os.Bundle
-import android.widget.ArrayAdapter
-import android.widget.ListView
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.smartbattery.details.R
 import com.smartbattery.details.receiver.ZebraBatteryReceiver
 import com.smartbattery.details.receiver.PanasonicBatteryReceiver
+import android.widget.TextView
+
+import android.widget.TableLayout
+
+import android.view.LayoutInflater
+import android.widget.Toast
+
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var stdApiTable: TableLayout
+    private lateinit var nonStdApiTable: TableLayout
+
+    private var zebraStdMap = mutableMapOf<String, String>()
+    private var zebraNonStdMap = mutableMapOf<String, String>()
+    private var panasonicStdMap = mutableMapOf<String, String>()
+    private var panasonicNonStdMap = mutableMapOf<String, String>()
+
     private var instance: MainActivity? = null
     private var zebraBatteryReceiver: BroadcastReceiver? = null
     private var panasonicBatteryReceiver: BroadcastReceiver? = null
-
-    var apiList = ArrayList<String>()
-    var adapter: ArrayAdapter<*>? = null
-
-    var health_percentage: String? = null
-    var battery_usage_numb: String? = null
-    var base_cumulative_charge: String? = null
-    var seconds_since_first_use: String? = null
-    var present_capacity: String? = null
-    var time_to_empty: String? = null
-    var time_to_full: String? = null
-    var present_charge: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         instance = this
 
-        val zebraApiList = findViewById<ListView>(R.id.api_list)
-        adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, apiList)
-        zebraApiList.adapter = adapter
-
         //Zebra  Battery info
         if (Build.MANUFACTURER.equals("Zebra Technologies", true)) {
             zebraBatteryReceiver = ZebraBatteryReceiver(instance!!)
             val zebraFilter = IntentFilter(Intent.ACTION_BATTERY_CHANGED)
-            val batteryStatus = registerReceiver(zebraBatteryReceiver, zebraFilter)
-            if (batteryStatus != null) handleZebraIntent(batteryStatus)
+            registerReceiver(zebraBatteryReceiver, zebraFilter)
         }
-
         //Panasonic  Battery info
         else if (Build.MANUFACTURER.equals("panasonic", true)) {
             panasonicBatteryReceiver = PanasonicBatteryReceiver(instance!!)
             val panasonicFilter = IntentFilter()
             panasonicFilter.addAction("com.panasonic.psn.batteryhealthcheck.api.RESPONSE")
-            val panasonicBatteryStatus = registerReceiver(panasonicBatteryReceiver, panasonicFilter)
-            if (panasonicBatteryStatus != null) handlePanasonicIntent(panasonicBatteryStatus)
+            registerReceiver(panasonicBatteryReceiver, panasonicFilter)
 
-            // Send “Execute” broadcast Intent after prior preparation
+            //  Send “Execute” broadcast Intent after prior preparation
             val intentExecute = Intent()
             intentExecute.action = "com.panasonic.psn.batteryhealthcheck.api.EXECUTE"
             sendBroadcast(intentExecute)
         } else
-            Toast.makeText(this, "Device not supported", Toast.LENGTH_LONG).show();
+            Toast.makeText(this, "Device not supported", Toast.LENGTH_LONG).show()
     }
 
     override fun onDestroy() {
@@ -74,74 +67,112 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun handleZebraIntent(intent: Intent) {
-        val mfg = intent.extras!!.getString("mfg", "unknown")
-        val partnumber = intent.extras!!.getString("partnumber", "unknown")
-        val serialnumber = intent.extras!!.getString("serialnumber", "unknown")
-        val ratedcapacity = intent.extras!!.getInt("ratedcapacity")
-        val battery_decommission = intent.extras!!.getInt("battery_decommission")
-        val total_cumulative_charge = intent.extras!!.getInt("total_cumulative_charge")
+        zebraNonStdMap["mfd"] = intent.extras!!.getString("mfd", "unknown")
+        zebraNonStdMap["partnumber"] = intent.extras!!.getString("partnumber", "unknown")
+        zebraStdMap["serialnumber"] = intent.extras!!.getString("serialnumber", "unknown")
+        zebraNonStdMap["ratedcapacity"] = intent.extras!!.getInt("ratedcapacity").toString()
+        zebraNonStdMap["battery_decommission"] =
+            intent.extras!!.getInt("battery_decommission",-1).toString()
+        zebraNonStdMap["total_cumulative_charge"] =
+            intent.extras!!.getInt("total_cumulative_charge").toString()
+
         if (getBatteryType(intent) == 202 || getBatteryType(intent) == 206) {
-            battery_usage_numb = intent.extras!!.getInt("battery_usage_numb").toString()
+            zebraStdMap["battery_usage_numb"] =
+                intent.extras!!.getInt("battery_usage_numb").toString()
         }
 
         if (getBatteryType(intent) == 201) {
-            base_cumulative_charge = intent.extras!!.getInt("base_cumulative_charge").toString()
-            seconds_since_first_use = intent.extras!!.getInt("seconds_since_first_use").toString()
-            present_capacity = intent.extras!!.getInt("present_capacity").toString()
-            health_percentage = intent.extras!!.getInt("health_percentage").toString()
-            time_to_empty = intent.extras!!.getInt("time_to_empty").toString()
-            time_to_full = intent.extras!!.getInt("time_to_full").toString()
-            present_charge = intent.extras!!.getInt("present_charge").toString()
+            zebraNonStdMap["base_cumulative_charge"] =
+                intent.extras!!.getInt("base_cumulative_charge").toString()
+            zebraNonStdMap["seconds_since_first_use"] =
+                intent.extras!!.getInt("seconds_since_first_use").toString()
+            zebraNonStdMap["present_capacity"] =
+                intent.extras!!.getInt("present_capacity").toString()
+            zebraNonStdMap["health_percentage"] =
+                intent.extras!!.getInt("health_percentage").toString()
+            zebraNonStdMap["time_to_empty"] = intent.extras!!.getInt("time_to_empty").toString()
+            zebraNonStdMap["time_to_full"] = intent.extras!!.getInt("time_to_full").toString()
+            zebraNonStdMap["present_charge"] =
+                intent.extras!!.getInt("present_charge").toString()
         }
-        apiList.clear()
-        apiList.add("=== STANDARD API ===")
-        apiList.add("Serial Number: " + serialnumber)
-        apiList.add("Battery usage number: " + battery_usage_numb)
 
-        apiList.add("=== NON STANDARD API ===")
-        apiList.add("Manufacture Date: " + mfg)
-        apiList.add("Part number: " + partnumber)
-        apiList.add("Rated capacity: " + ratedcapacity)
-        apiList.add("Battery decommission: " + battery_decommission)
-        apiList.add("Base cumulative charge: " + base_cumulative_charge)
-        apiList.add("Seconds since first use: " + seconds_since_first_use)
-        apiList.add("Total cumulative charge: " + total_cumulative_charge)
-        apiList.add("Present Capacity:" + present_capacity)
-        apiList.add("Time to empty charged: " + time_to_empty)
-        apiList.add("Time to fully charged: " + time_to_full)
-        apiList.add("Health percentage: " + health_percentage)
-        apiList.add("Present_charge: " + present_charge)
-        adapter?.notifyDataSetChanged()
+        stdApiTable = findViewById(R.id.standard_api_table)
+        stdApiTable.removeAllViews()
+        nonStdApiTable = findViewById(R.id.non_standard_api_table)
+        nonStdApiTable.removeAllViews()
+
+        val inflater = this.getSystemService(LAYOUT_INFLATER_SERVICE) as LayoutInflater
+        val stdTitleI = inflater.inflate(R.layout.table_title, null, false)
+        stdTitleI.findViewById<TextView>(R.id.header_title).text = "STANDARD API"
+        stdApiTable.addView(stdTitleI)
+
+        for ((key, value) in zebraStdMap) {
+            val tableRowI = inflater.inflate(R.layout.table_row, null, false)
+            val keyDI = tableRowI.findViewById<TextView>(R.id.key_data)
+            val valueDI = tableRowI.findViewById<TextView>(R.id.value_data)
+            keyDI.text = key
+            valueDI.text = value
+            stdApiTable.addView(tableRowI)
+        }
+
+        val nonStdTitleI = inflater.inflate(R.layout.table_title, null, false)
+        nonStdTitleI.findViewById<TextView>(R.id.header_title).text = "NON STANDARD API"
+        nonStdApiTable.addView(nonStdTitleI)
+
+        for ((key, value) in zebraNonStdMap) {
+            val tableRowI = inflater.inflate(R.layout.table_row, null, false)
+            val keyDI = tableRowI.findViewById<TextView>(R.id.key_data)
+            val valueDI = tableRowI.findViewById<TextView>(R.id.value_data)
+            keyDI.text = key
+            valueDI.text = value
+            nonStdApiTable.addView(tableRowI)
+        }
     }
 
     fun handlePanasonicIntent(intent: Intent) {
-        val serial = intent.getStringExtra("serial") ?: "unknown"
-        val date = intent.getStringExtra("product_date") ?: "unknown"
-        val health = intent.getIntExtra("health", 0)
-        val count = intent.getIntExtra("count", 0)
-
+        panasonicStdMap["serial"] = intent.getStringExtra("serial") ?: "unknown"
+        panasonicNonStdMap["product_date"] = intent.getStringExtra("product_date") ?: "unknown"
+        panasonicNonStdMap["health"] = intent.getIntExtra("health", 0).toString()
+        panasonicStdMap["count"] = intent.getIntExtra("count", 0).toString()
         //FZ-A3 Only from here
-        val remaining = intent.getIntExtra("remaining", 0)
-        val serial_2 = intent.getStringExtra("serial2") ?: "unknown"
-        val date_2 = intent.getStringExtra("product_date2") ?: "unknown"
-        val health_2 = intent.getIntExtra("health2", 0)
-        val count_2 = intent.getIntExtra("count2", 0)
-        val remaining_2 = intent.getIntExtra("remaining2", 0)
+        panasonicNonStdMap["remaining"] = intent.getIntExtra("remaining", 0).toString()
+        panasonicStdMap["serial2"] = intent.getStringExtra("serial2") ?: "unknown"
+        panasonicNonStdMap["product_date2"] = intent.getStringExtra("product_date2") ?: "unknown"
+        panasonicNonStdMap["health2"] = intent.getIntExtra("health2", 0).toString()
+        panasonicStdMap["count2"] = intent.getIntExtra("count2", 0).toString()
+        panasonicNonStdMap["remaining2"] = intent.getIntExtra("remaining2", 0).toString()
 
-        apiList.clear()
-        apiList.add("=== STANDARD API ===")
-        apiList.add("Serial Number: " + serial)
-        apiList.add("Serial Number2: " + serial_2)
-        apiList.add("Cycle Count : " + count)
-        apiList.add("Cycle Count2 : " + count_2)
+        stdApiTable = findViewById(R.id.standard_api_table)
+        stdApiTable.removeAllViews()
 
-        apiList.add("=== NON STANDARD API ===")
-        apiList.add("Manufacture Date: " + date)
-        apiList.add("Manufacture Date2: " + date_2)
-        apiList.add("Health percentage: " + health)
-        apiList.add("Health percentage2: " + health_2)
-        apiList.add("Remaining Battery : " + remaining)
-        apiList.add("Remaining Battery2 : " + remaining_2)
-        adapter?.notifyDataSetChanged()
+        nonStdApiTable = findViewById(R.id.non_standard_api_table)
+        nonStdApiTable.removeAllViews()
+
+        val inflater = this.getSystemService(LAYOUT_INFLATER_SERVICE) as LayoutInflater
+        val stdTitleI = inflater.inflate(R.layout.table_title, null, false)
+        stdTitleI.findViewById<TextView>(R.id.header_title).text = "STANDARD API"
+        stdApiTable.addView(stdTitleI)
+
+        for ((key, value) in panasonicStdMap) {
+            val tableRowI = inflater.inflate(R.layout.table_row, null, false)
+            val keyDI = tableRowI.findViewById<TextView>(R.id.key_data)
+            val valueDI = tableRowI.findViewById<TextView>(R.id.value_data)
+            keyDI.text = key
+            valueDI.text = value
+            stdApiTable.addView(tableRowI)
+        }
+
+        val nonStdTitleI = inflater.inflate(R.layout.table_title, null, false)
+        nonStdTitleI.findViewById<TextView>(R.id.header_title).text = "NON STANDARD API"
+        nonStdApiTable.addView(nonStdTitleI)
+
+        for ((key, value) in panasonicNonStdMap) {
+            val tableRowI = inflater.inflate(R.layout.table_row, null, false)
+            val keyDI = tableRowI.findViewById<TextView>(R.id.key_data)
+            val valueDI = tableRowI.findViewById<TextView>(R.id.value_data)
+            keyDI.text = key
+            valueDI.text = value
+            nonStdApiTable.addView(tableRowI)
+        }
     }
 }
